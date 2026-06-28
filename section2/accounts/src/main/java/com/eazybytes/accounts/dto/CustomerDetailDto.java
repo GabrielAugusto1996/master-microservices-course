dbf44c0770663f21952b3d1193703f2067b9acbe
Gabriel Morato <gabriel.morato139@gmail.com>
2026-06-28 13:32:17 +0200

    Perform input data validations inside accounts microservice

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDetailDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDetailDto.java
@@ -2,10 +2,12 @@ package com.eazybytes.accounts.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-public record CustomerDetailDto(@NotBlank String name,
+public record CustomerDetailDto(@NotBlank @Size(min = 5, max = 30) String name,
                                 @NotBlank @Email String email,
-                                @NotBlank String mobileNumber,
+                                @NotBlank @Pattern(regexp = "(^$|[0-9]{9})") String mobileNumber,
                                 @NotBlank String accountType,
                                 @NotBlank String branchAddress) {
 }
--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -3,12 +3,14 @@ package com.eazybytes.accounts.dto;
 import com.eazybytes.accounts.entity.Customer;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
-public record CustomerDto(@NotBlank String name,
+public record CustomerDto(@NotBlank @Size(min = 5, max = 30) String name,
                           @NotBlank @Email String email,
-                          @NotBlank String mobileNumber,
+                          @NotBlank  @Pattern(regexp = "(^$|[0-9]{9})", message = "Mobile number must be 10 digits") String mobileNumber,
                           AccountDto accountDto) {
 
     public Customer toEntity() {
