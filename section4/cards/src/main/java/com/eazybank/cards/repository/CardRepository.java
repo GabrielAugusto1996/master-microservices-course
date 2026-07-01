3f9ccfc557b211ec621ba1efc63d99835bcf1904
Gabriel Morato <gabriel.morato139@gmail.com>
2026-07-01 13:59:10 +0200

    feat: Generate Docker Image of Cards microservice with GoogleJib

--- section4/cards/src/main/java/com/eazybank/cards/repository/CardRepository.java
+++ section4/cards/src/main/java/com/eazybank/cards/repository/CardRepository.java
@@ -1,6 +1,6 @@
 package com.eazybank.cards.repository;
 
-import com.eazybank.cards.entity.Loan;
+import com.eazybank.cards.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
