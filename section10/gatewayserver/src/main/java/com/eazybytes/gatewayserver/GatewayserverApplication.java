5ac6d541ee0823ac1784558e7f78eb3c67e82586
Gabriel Morato <gabriel.morato139@gmail.com>
2026-07-12 13:28:48 +0200

    feat: Implementing Retry pattern in Gateway

--- section10/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ section10/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -5,47 +5,62 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
-		return routeLocatorBuilder.routes()
-				.route(p -> p
-						.path("/eazybank/accounts/**")
-						.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-								.circuitBreaker(config -> {
-									config.setName("accountsCircuitBreaker");
-									config.setFallbackUri("/contactSupport");
-								}))
-						.uri("lb://ACCOUNTS"))
-				.route(p -> p
-						.path("/eazybank/loans/**")
-						.filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-								.circuitBreaker(config -> {
-									config.setName("loansCircuitBreaker");
-								}))
-						.uri("lb://LOANS"))
-				.route(p -> p
-						.path("/eazybank/cards/**")
-						.filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-								.circuitBreaker(config -> {
-									config.setName("cardsCircuitBreaker");
-								}))
-						.uri("lb://CARDS")).build();
+    @Bean
+    public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/eazybank/accounts/**")
+                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> {
+                                    config.setName("accountsCircuitBreaker");
+                                    config.setFallbackUri("/contactSupport");
+                                }))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/eazybank/loans/**")
+                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(r ->
+                                        r.setRetries(3)
+                                                .setMethods(
+                                                        HttpMethod.GET,
+                                                        HttpMethod.DELETE
+                                                )
+                                                .setBackoff(
+                                                        Duration.ofMillis(100),
+                                                        Duration.ofMillis(1000),
+                                                        2,
+                                                        true
+                                                )
+                                )
+                                .circuitBreaker(config -> {
+                                    config.setName("loansCircuitBreaker");
+                                }))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/eazybank/cards/**")
+                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> {
+                                    config.setName("cardsCircuitBreaker");
+                                }))
+                        .uri("lb://CARDS")).build();
 
 
-	}
+    }
 
 
 }
