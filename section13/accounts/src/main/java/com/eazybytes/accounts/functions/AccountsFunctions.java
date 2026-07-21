da418ebf8cc6510cf1fecd62bb87b22ea174889c
Gabriel Morato <gabriel.morato139@gmail.com>
2026-07-21 16:27:39 +0200

    feat: Demo of Async communication or event streaming using Rabbit MQ - Part 2

---
+++ section13/accounts/src/main/java/com/eazybytes/accounts/functions/AccountsFunctions.java
@@ -0,0 +1,23 @@
+package com.eazybytes.accounts.functions;
+
+import com.eazybytes.accounts.service.AccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class AccountsFunctions {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsFunctions.class);
+
+    @Bean
+    public Consumer<Long> updateCommunication(AccountService accountService) {
+        return accountNumber -> {
+            accountService.deleteAccountByAccountNumber(accountNumber);
+            logger.info("Account number was deleted: {}", accountNumber);
+        };
+    }
+}
